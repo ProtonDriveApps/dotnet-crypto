c82f91ee9eef579ad4044f368f56cc4aa1dbee54
drive <drive@proton.ch>
2025-01-14 14:57:35 +0100

    Add extension to export public key to bytes

--- src/dotnet/Pgp/PgpKeyExtensions.cs
+++ src/dotnet/Pgp/PgpKeyExtensions.cs
@@ -1,12 +1,21 @@
 ﻿namespace Proton.Cryptography.Pgp;
 
-public static class PgpPrivateKeyExtensions
+public static class PgpKeyExtensions
 {
     public static ArraySegment<byte> ToBytes(this PgpPrivateKey privateKey, PgpEncoding encoding = default)
     {
         using var stream = new MemoryStream();
 
         privateKey.Export(stream, encoding);
+
+        return stream.TryGetBuffer(out var buffer) ? buffer : stream.ToArray();
+    }
+
+    public static ArraySegment<byte> ToBytes(this PgpPublicKey publicKey, PgpEncoding encoding = default)
+    {
+        using var stream = new MemoryStream();
+
+        publicKey.Export(stream, encoding);
 
         return stream.TryGetBuffer(out var buffer) ? buffer : stream.ToArray();
     }
